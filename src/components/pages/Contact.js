fcd0264396e47496d223c0b1ab9feaff28eb6e3d
alexisbenavidez99 <callmealexis49@gmail.com>
2023-06-06 19:18:20 -0500

    added swiper and redesigning projects page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -47,82 +47,82 @@ export default function Contact() {
 
     return (
         <motion.div
-                initial={{ opacity: 0, backgroundColor: "black" }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.5, duration: 1.5 }}
-            >
-        <Container className='contact'>
-            <Row className="mb-5 mt-3">
-                <Col lg='8'>
-                    <h1 className="mb-4 contact-header">
-                        Contact Me
-                    </h1>
-                </Col>
-            </Row>
+            initial={{ opacity: 0, backgroundColor: "black" }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 1.5 }}
+        >
+            <Container className='contact'>
+                <Row className="mb-5 mt-3">
+                    <Col lg='8'>
+                        <h1 className="mb-4 contact-header">
+                            Contact Me
+                        </h1>
+                    </Col>
+                </Row>
 
-            <Row className="sec_sp">
-                <Col lg='5' className="mb-5">
-                    <h3 className='color_sec py-4 contact-info'>Get in touch</h3>
-                    <address className="contact-info">
-                        <AiOutlineMail />
-                        <strong className='pt-2'> alexisobenavidez@gmail.com</strong>
-                        <p className='pt-4'>
-                            <AiOutlinePhone />
-                            <strong> 615-956-1191</strong>
-                        </p>
-                        <p className='pt-2'>
-                            <BiMap />
-                            <strong> Murfreesboro, Tennessee</strong>
-                        </p>
-                        <p className='pt-2'>
-                            <AiOutlineGithub />
-                            <strong> alexisbenavidez99</strong>
-                        </p>
-                    </address>
-                </Col>
-                <Col lg='7' className='d-flex align-items-center'>
-                    <form className='contact_form w-100'>
-                        <Row>
-                            <Col lg='6' className='form-group mb-3'>
-                                <input
-                                    className='form-control'
-                                    id='name'
-                                    name='name'
-                                    placeholder='Name'
-                                    type='text'
-                                    value={name}
-                                    onChange={handleInputChange}
-                                />
-                            </Col>
-                            <Col lg='6' className='form-group mb-3'>
-                                <input
-                                    className='form-control'
-                                    id='email'
-                                    name='email'
-                                    placeholder='Email'
-                                    type='email'
-                                    value={email}
-                                    onChange={handleInputChange}
-                                />
-                            </Col>
-                        </Row>
-                        <textarea className='form-control' id='message'
-                                    name='message'
-                                    placeholder='Message'
-                                    rows='5'
-                                    value={message}
-                                    onChange={handleInputChange}
-                        ></textarea>
-                        <br />
-                        <Row>
-                            <Col lg='12' className='form-group'>
-                                <button className='button' type='submit' onClick={handleFormSubmit}>Send</button>
-                            </Col>
-                        </Row>
-                    </form>
-                </Col>
-            </Row>
-        </Container>
+                <Row className="sec_sp">
+                    <Col lg='5' className="mb-5">
+                        <h3 className='color_sec py-4 contact-info'>Get in touch</h3>
+                        <address className="contact-info">
+                            <AiOutlineMail />
+                            <strong className='pt-2'> alexisobenavidez@gmail.com</strong>
+                            <p className='pt-4'>
+                                <AiOutlinePhone />
+                                <strong> 615-956-1191</strong>
+                            </p>
+                            <p className='pt-2'>
+                                <BiMap />
+                                <strong> Murfreesboro, Tennessee</strong>
+                            </p>
+                            <p className='pt-2'>
+                                <AiOutlineGithub />
+                                <strong> alexisbenavidez99</strong>
+                            </p>
+                        </address>
+                    </Col>
+                    <Col lg='7' className='d-flex align-items-center'>
+                        <form className='contact_form w-100'>
+                            <Row>
+                                <Col lg='6' className='form-group mb-3'>
+                                    <input
+                                        className='form-control'
+                                        id='name'
+                                        name='name'
+                                        placeholder='Name'
+                                        type='text'
+                                        value={name}
+                                        onChange={handleInputChange}
+                                    />
+                                </Col>
+                                <Col lg='6' className='form-group mb-3'>
+                                    <input
+                                        className='form-control'
+                                        id='email'
+                                        name='email'
+                                        placeholder='Email'
+                                        type='email'
+                                        value={email}
+                                        onChange={handleInputChange}
+                                    />
+                                </Col>
+                            </Row>
+                            <textarea className='form-control' id='message'
+                                name='message'
+                                placeholder='Message'
+                                rows='5'
+                                value={message}
+                                onChange={handleInputChange}
+                            ></textarea>
+                            <br />
+                            <Row>
+                                <Col lg='12' className='form-group'>
+                                    <button className='button' type='submit' onClick={handleFormSubmit}>Send</button>
+                                </Col>
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
         </motion.div>
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import { React, useRef, useEffect } from 'react';
 import longevity from '../../assets/longevity.png';
 import coinspire from '../../assets/coinspire.png';
 import weather from '../../assets/weather-app.png';
 import bookHaven from '../../assets/bookhaven.png';
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import { register } from "swiper/element/bundle";
 import { motion } from "framer-motion";
 
+register();
+
 export default function Projects() {
+    const swiperElRef = useRef(null);
+
+  useEffect(() => {
+    // listen for Swiper events using addEventListener
+    swiperElRef.current.addEventListener('progress', (e) => {
+      const [swiper, progress] = e.detail;
+      console.log(progress);
+    });
+
+    swiperElRef.current.addEventListener('slidechange', (e) => {
+      console.log('slide changed');
+    });
+  }, []);
+
     return (
         <div className="projects">
-            <motion.div
+            {/* <motion.div
                 className="projects"
                 initial={{ opacity: 0, backgroundColor: "black" }}
                 animate={{ opacity: 1 }}
@@ -44,8 +65,95 @@ export default function Projects() {
                     </a>
                     <h3>Book Haven</h3>
                     <p><a href="https://github.com/Nmotley92/e-commerce-site">GH</a></p>
-                </div>
-            </motion.div>
+                </div> */}
+                <section className='portfolio-section' id='portfolio'>
+                    <h2 className='section-title'>Projects</h2>
+                    
+                    <Swiper className='projects-container container swiper-container'
+                    ref={swiperElRef}
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    speed={500}
+                    loop={true}
+                    touchRatio={1.5}
+                    navigation={true}
+                    pagination={true}
+                    >
+                        <div className='swiper-wrapper'>
+                            {/* Project 1 */}
+                            <SwiperSlide className='project-content grid'>
+                            <img src={longevity} className='project-img' alt='Longevity' />
+
+                            <div className='project-data'>
+                                <h3 className='project-title'>Longevity</h3>
+                                <p className='project-description'>A full-stack application that allows users to create a profile, curate workout plans, learn new exercises with instructional descriptions of exercises they choose, and keep track of their past workouts.</p>
+                                <a href="https://rocky-thicket-55053.herokuapp.com/" target="_blank" rel="noreferrer" className='button button--flex button--small portfolio-button'>
+                                    Visit Site 
+                                    <AiOutlineArrowRight />
+                                </a>
+                                <a href="https://github.com/alexisbenavidez99/workout-tracker" className='button button--flex button--small portfolio-button'>
+                                <AiOutlineGithub />
+                                    GitHub
+                                </a>
+                            </div>
+                            </SwiperSlide>
+
+                            {/* Project 2 */}
+                            <SwiperSlide className='project-content grid'>
+                            <img src={coinspire} className='project-img' alt='Coinspire' />
+
+                            <div className='project-data'>
+                                <h3 className='project-title'>Coinspire</h3>
+                                <p className='project-description'>CoinSpire is a front-end web application that allows users to live track current crypto-currency prices to assist and aid beginners at cryptocurrency in fast trading, accurate data for learning, with easy and simple UI, and users will be able to see only the important details.</p>
+                                <a href="https://devkjoon.github.io/coinspire/" target="_blank" rel="noreferrer" className='button button--flex button--small portfolio-button'>
+                                    Visit Site 
+                                    <AiOutlineArrowRight />
+                                </a>
+                                <a href="https://github.com/devkjoon/coinspire" className='button button--flex button--small portfolio-button'>
+                                <AiOutlineGithub />
+                                    GitHub
+                                </a>
+                            </div>
+                            </SwiperSlide>
+
+                            {/* Project 3 */}
+                            <SwiperSlide className='project-content grid'>
+                            <img src={weather} className='project-img' alt='Weather App' />
+
+                            <div className='project-data'>
+                                <h3 className='project-title'>Weather App</h3>
+                                <p className='project-description'>A weather app that uses OpenWeatherMap APIs to create an app for people to plan trips accordingly.</p>
+                                <a href="https://alexisbenavidez99.github.io/Weather-App-6/" target="_blank" rel="noreferrer" className='button button--flex button--small portfolio-button'>
+                                    Visit Site 
+                                    <AiOutlineArrowRight />
+                                </a>
+                                <a href="https://github.com/alexisbenavidez99/Weather-App-6" className='button button--flex button--small portfolio-button'>
+                                <AiOutlineGithub />
+                                    GitHub
+                                </a>
+                            </div>
+                            </SwiperSlide>
+
+                            {/* Project 4 */}
+                            <SwiperSlide className='project-content grid'>
+                            <img src={bookHaven} className='project-img' alt='Book Haven' />
+
+                            <div className='project-data'>
+                                <h3 className='project-title'>Book Haven</h3>
+                                <p className='project-description'>A MERN stack book e-commerce website that includes detailed descriptions, customer reviews, and recommendations to help guide your choices.</p>
+                                <a href="https://book-haven.herokuapp.com/" target="_blank" rel="noreferrer" className='button button--flex button--small portfolio-button'>
+                                    Visit Site 
+                                    <AiOutlineArrowRight />
+                                </a>
+                                <a href="https://github.com/Nmotley92/e-commerce-site" className='button button--flex button--small portfolio-button'>
+                                <AiOutlineGithub />
+                                    GitHub
+                                </a>
+                            </div>
+                            </SwiperSlide>
+                        </div>
+                    </Swiper>
+                </section>
         </div>
     )
 }
